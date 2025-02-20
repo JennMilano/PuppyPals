b9abc66fd4f91f90d350993dd6bcbf79e89a2f26
Jenn Milano <jmilano0354@gmail.com>
2025-02-20 12:52:05 -0500

    initial changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,49 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
 import './App.css'
+import './index.css'
 
-<div></div>
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+
+  console.log(featuredPup) 
+
+  function handleClick() {
+    // some logic here
+  }
+
+  return (
+ 
+    <div className="App">
+
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={() => {
+            console.log("puppy id: ", puppy.id);  // Log the puppy ID
+            setFeatPupId(puppy.id);  // Set the featured puppy ID
+          }}
+          key={puppy.id}
+        >
+          {puppy.name}
+          </p>
+        );
+      })}
+
+{featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+
+  );
+}
+
 
 export default App
